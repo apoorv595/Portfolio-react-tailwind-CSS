140960b154fb0ecc931d081cdb2f745a74d3e6be
apoorv595 <apoorvgupta595@gmail.com>
2024-07-21 00:28:24 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,35 +3,35 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate frontend developer with a knack for crafting robust and scalable web applications. With  years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like  MySQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile frontend  developer with a passion for creating efficient and user-friendly web applications. With years of  experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
+    year: "2023 -2024",
+    role: "Frontend Developer(Using react js ,Tailwind CSS),Built e-commerce website(Spotify Clone),Java",
     company: "Google Inc.",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
     year: "2022 - 2023",
-    role: "Frontend Developer",
+    role: "clone(Gemini),Core Java(Connect four)",
     company: "Adobe",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
+    role: "Block Pool(Block Chain)",
     company: "Facebook",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
   {
     year: "2020 - 2021",
-    role: "Software Engineer",
+    role: "Built basic food web(Html,CSS,JS,SQL)",
     company: "Paypal",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
@@ -44,28 +44,28 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React"],
   },
   {
-    title: "Task Management App",
+    title: "Connect Four Game:",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Developed a robust and engaging desktop application for playing the popular Connect Four game,implementing various features and functionalities to enhance the user experience. Utilized Java FX to create an interactive and user-friendly UI, showcasing the game board with smooth transition as discs fall",
+    technologies: [" Java", " JavaFX", "Sql"],
   },
   {
-    title: "Portfolio Website",
+    title: "Portfolio Website:",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "University Management System:",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Developed a comprehensive University Management System, Tested efficient management of over 100 students, 10 faculty members, and 5 courses• Engineered a reliable data layer using MySQL, implementing normalized database schemas to assure data integrity and efficient query operation achieving a 100% data accuracy rate.",
+    technologies: [" Java", "Java Swing", "MySql"],
   },
 ];
 
